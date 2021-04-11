d13826df419cea578d4897790cdb505e3bc409c5
unknown <boltcode17@gmail.com>
2021-04-11 08:13:22 -0700

    new update

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -3,11 +3,18 @@ pragma solidity ^0.5.0;
 contract DappToken {
     string public name = "DApp Token";
     string public symbol = "DAPP";
-    //symbol
     uint256 public totalSupply = 1000000;
+
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    // allowance
 
     event Transfer(address indexed _from, address indexed _to, uint256 amount);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
     constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
@@ -19,7 +26,46 @@ contract DappToken {
     {
         require(balanceOf[msg.sender] >= _value);
 
-        Transfer(msg.sender, _to, _value);
+        emit Transfer(msg.sender, _to, _value);
         // Transfer Event
+
+        return true;
+    }
+
+    // Delegated Transfers
+
+    // approve
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+        // Approve event
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    // transferFrom
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        // Require _from has enough tokens
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        // Require allowance is big enough
+
+        //Change th balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        // Update the allowance
+        allowance[_from][msg.sender] -= _value;
+        
+        // Transfer Event
+        Transfer(_from, _to, _value);
+        return true;
     }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -40,41 +40,150 @@ var DappToken = artifacts.require("DappToken");
 //   });
 // });
 
-it("transfers token ownership", function () {
-  return DappToken.deployed()
-    .then(function (instance) {
-      tokenInstance = instance;
+// it("transfers token ownership", function () {
+//   return DappToken.deployed()
+//     .then(function (instance) {
+//       tokenInstance = instance;
 
-      return tokenInstance.transfers.call(accounts[1], 999999999);
-    })
-    .then(assert.fail)
-    .catch(function (error) {
-      assert(
-        error.message.indexOf("revert") >= 0,
-        "error message must contain revert"
-      );
+//       return tokenInstance.transfers.call(accounts[1], 999999999);
+//     })
+//     .then(assert.fail)
+//     .catch(function (error) {
+//       assert(
+//         error.message.indexOf("revert") >= 0,
+//         "error message must contain revert"
+//       );
 
-      return tokenInstance.transfer(accounts[1], 25, { from: accounts[0] });
-    })
-    .then(function (receipt) {
-      return tokenInstance.balanceOf(accounts[1]);
-    })
-    .then(function (balance) {
-      assert.equal(
-        balance.toNumber(),
-        250000,
-        "adds the amount to the receiving account"
-      );
-      return tokenInstance.balanceOf(accounts[0]);
-    })
-    .then(function (balance) {
-      assert.equal(
-        balance.toNumber(),
-        750000,
-        "deducts the amount from the sending account"
-      );
+//       return tokenInstance.transfer(accounts[1], 25, { from: accounts[0] });
+//     })
+//     .then(function (receipt) {
+//       return tokenInstance.balanceOf(accounts[1]);
+//     })
+//     .then(function (balance) {
+//       assert.equal(
+//         balance.toNumber(),
+//         250000,
+//         "adds the amount to the receiving account"
+//       );
+//       return tokenInstance.balanceOf(accounts[0]);
+//     })
+//     .then(function (balance) {
+//       assert.equal(
+//         balance.toNumber(),
+//         750000,
+//         "deducts the amount from the sending account"
+//       );
 
-      balanceOf[msg.sender] -= _value;
-      balanceOf[_to] += _value;
-    });
+//       balanceOf[msg.sender] -= _value;
+//       balanceOf[_to] += _value;
+//     });
+// });
+
+// contract("MyContract", function (accounts) {
+//   it("approves tokens from delegated transfer", function () {
+//     return DappToken.deployed()
+//       .then((instance) => {
+//         tokenInstance = instance;
+//         return tokenInstance.approve.call(accounts[1], 100);
+//       })
+//       .then(function (success) {
+//         assert.equal(success, true, "it returns true");
+//       });
+//   });
+// });
+
+contract("MyContract", function (accounts) {
+  it("approves tokens from delegated transfer", function () {
+    return DappToken.deployed()
+      .then((instance) => {
+        tokenInstance = instance;
+        return tokenInstance.approve.call(accounts[1], 100);
+      })
+      .then(function (success) {
+        assert.equal(success, true, "it returns true");
+        return tokenInstance.approve(accounts[1], 100, { from: accounts[0] });
+      })
+      .then(function (receipt) {
+        assert.equal(receipt.logs.length, 1, "triggers one event");
+
+        assert.equal(
+          receipt.logs[0].event,
+          "Approval",
+          "should be the 'Approval' event"
+        );
+
+        assert.equal(
+          receipt.logs[0].args._owner,
+          accounts[0],
+          "logs the account the tokens are authorized by"
+        );
+
+        assert.equal(
+          receipt.logs[0].args._spender,
+          accounts[1],
+          "logs the account the tokens are authorized to"
+        );
+
+        assert.equal(receipt.logs[0]._value, 100, "logs the transfer amount");
+        return tokenInstance.allowance(accounts[0], accounts[1]);
+      })
+      .then(function (allowance) {
+        assert.equal(
+          allowance.toNumber(),
+          100,
+          "stores the allowance for delegated transfer"
+        );
+      });
+  });
+
+  it("handles delegate transfer", function () {
+    return DappToken.deployed()
+      .then(function (instance) {
+        tokenInstance = instance;
+
+        fromAccount = accounts[2];
+        toAccount = accounts[3];
+        spendingAccount = accounts[4];
+
+        // Transfer some tokens to fromAccount
+        return tokenInstance.transfer(fromAccount, 100, { from: accounts[0] });
+      })
+      .then((receipt) => {
+        // Approve spending account to spend 10 tokens from fromAccount
+        return tokenInstance
+          .approve(spendingAccount, 10, {
+            from: fromAccount,
+          })
+          .then(function (receipt) {
+            // Try traffering something larger than the snder's blance
+            return tokenInstance.transferFrom(fromAccount, toAccount, 1000);
+          })
+          .then(assert.fail)
+          .catch((error) => {
+            assert(
+              error.message.indexOf("revert") >= 0,
+              "cannot transfer a value lareger than blance"
+            );
+            return tokenInstance
+              .transferFrom(fromAccount, toAccount, 20, {
+                from: spendingAccount,
+              })
+              .then(assert.fail)
+              .catch((erorr) => {
+                assert(
+                  error.message.indexOf("revert") >= 0,
+                  "cannot transfer a value lareger than approved amount"
+                );
+
+                return tokenInstance
+                  .transferFrom(fromAccount, toAccount, 10, {
+                    from: spendingAccount,
+                  })
+                  .then((success) => {
+                    assert.equal(success, true);
+                  });
+              });
+          });
+      });
+  });
 });
